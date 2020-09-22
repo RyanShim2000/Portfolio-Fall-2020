b25c9aa11ccb58dcc4667dfd9ae9aeec375f986c
fakeshotz <fakeshotz100@gmail.com>
2020-09-22 02:17:28 -0400

    Updates

---
+++ precache-manifest.8959bfe13f9796749b5d570b8f7e24a4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35398110b88cc103732bbc808085f78",
+    "url": "/Portfolio-Fall-2020/index.html"
+  },
+  {
+    "revision": "98cf301737c6b310d0e0",
+    "url": "/Portfolio-Fall-2020/static/css/main.b6bca53d.chunk.css"
+  },
+  {
+    "revision": "28401f5a2bf1a20ce959",
+    "url": "/Portfolio-Fall-2020/static/js/2.15f4d5fb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio-Fall-2020/static/js/2.15f4d5fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98cf301737c6b310d0e0",
+    "url": "/Portfolio-Fall-2020/static/js/main.fe8a5904.chunk.js"
+  },
+  {
+    "revision": "6b31b0b2c297f03016bd",
+    "url": "/Portfolio-Fall-2020/static/js/runtime-main.df223ab3.js"
+  },
+  {
+    "revision": "f123cf49100b7a9d39d2ff5021c90f21",
+    "url": "/Portfolio-Fall-2020/static/media/contactme.f123cf49.jpg"
+  },
+  {
+    "revision": "c74bf81a9c3c5c8797619b368c60ec95",
+    "url": "/Portfolio-Fall-2020/static/media/datasci.c74bf81a.jpg"
+  },
+  {
+    "revision": "383e64acb8f873071a472773e2b7b5c7",
+    "url": "/Portfolio-Fall-2020/static/media/django.383e64ac.jpg"
+  },
+  {
+    "revision": "1682ecb552800243328bfa79226b646c",
+    "url": "/Portfolio-Fall-2020/static/media/homeimg.1682ecb5.jpg"
+  },
+  {
+    "revision": "4f12bf35a72b51fc7fec293a307fe41f",
+    "url": "/Portfolio-Fall-2020/static/media/resume_fall_2020.4f12bf35.pdf"
+  },
+  {
+    "revision": "6f129584ae08af4623b118268aaef3e3",
+    "url": "/Portfolio-Fall-2020/static/media/solitaire.6f129584.jpg"
+  },
+  {
+    "revision": "1d91051d7601a57f295a2ef8ff47d765",
+    "url": "/Portfolio-Fall-2020/static/media/weather.1d91051d.jpg"
+  }
+]);
